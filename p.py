bf5086f8b4efd8c627b78e4f6e8e92fe4d5b67ab
hehaichi <hehaichi@gmail.com>
2015-06-26 16:17:05 +0530

    Some polishing

--- p.py
+++ p.py
@@ -3,99 +3,59 @@ import sys
 import time
 import threading
 class Server(threading.Thread):
-    """def __init__(self,sock=None):
-        if sock is None:
-            self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
-        else:
-            self.sock=sock"""
-    
     def run(self):
-    #self.l.acquire()
         self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        print "program control is in server"
+        print "Server started successfully\n"
         hostname=''
         port=51412
         self.sock.bind((hostname,port))
         self.sock.listen(1)
-        print "Listening on port %d" %port
-        #chunks=[]
-        time.sleep(2)
-    #self.l.release()
+        print "Listening on port %d\n" %port        
+        #time.sleep(2)    
         (clientname,address)=self.sock.accept()
-        print "Connection from %s" % str(address)
-        
+        print "Connection from %s\n" % str(address)        
         while 1:
-            chunk=clientname.recv(4096)
-            """if not chunk:
-                print"Error:Socket connection broken"
-                break"""
-            #chunks.append(chunk)
-            print chunk
-            #clientname.close()
-        #print ''.join(chunks)
+            chunk=clientname.recv(4096)            
+            print str(address)+':'+chunk 
 
-    """def main(self):
-        
-        self.run()"""
-
-class Client(threading.Thread):
-    """def __init__(self,l,sock=None):
-        if sock is None:
-            self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            self.l=l
-        else:
-            self.sock=sock"""
+class Client(threading.Thread):    
     def connect(self,host,port):
         self.sock.connect((host,port))
-    def client(self,host,port,msg):
-        print "Connecting"
-        #self.connect(host,port)
-        print "Connected"
-        #msg=raw_input('')
-        totalsent=0
-        #while totalsent<len(msg):
-        sent=self.sock.send(msg)
-            #"""if sent ==0:
-            #    raise RuntimeError("broken connection")
-            #totalsent+=sent"""
-    #print "Socket closed"
-        #self.sock.close()
-
+    def client(self,host,port,msg):               
+        sent=self.sock.send(msg)           
+        print "Sent\n"
     def run(self):
         self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         try:
-            host=raw_input("Enter the hostname\n")
-            print host
-            port=int(raw_input("Enter the port\n"))
+            host=raw_input("Enter the hostname\n>>")            
+            port=int(raw_input("Enter the port\n>>"))
         except EOFError:
             print "Error"
             return 1
         
-        
+        print "Connecting\n"
         s=''
         self.connect(host,port)
-        while 1:
-            #p2p=Client()
-            print "Waiting for msg"
-            msg=raw_input('')
+        print "Connected\n"
+        while 1:            
+            print "Waiting for message\n"
+            msg=raw_input('>>')
             if msg=='exit':
                 break
-            print "Sending"
+            if msg=='':
+                continue
+            print "Sending\n"
             self.client(host,port,msg)
-
-def srvr():
-    srv=Server()
-    srv.start()
-def client():
-    cli=Client()
-    cli.start()
-#def printLogs()
+        return(1)
 if __name__=='__main__':
     srv=Server()
     srv.daemon=True
+    print "Starting server"
     srv.start()
+    time.sleep(1)
+    print "Starting client"
     cli=Client()
+    print "Started successfully"
     cli.start()
     
     
