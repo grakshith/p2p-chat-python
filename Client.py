08e7b9a4132fbba4c017d392f20febf41007df93
hehaichi <hehaichi@gmail.com>
2016-06-14 14:55:33 +0530

    IEEE Networking Assignment

--- Client.py
+++ Client.py
@@ -4,7 +4,7 @@ import socket
 import sys
 import time
 import threading
-import select
+import select, signal
 import traceback
 class Server(threading.Thread):
     def initialise(self,receive):
@@ -16,10 +16,10 @@ class Server(threading.Thread):
             read,write,err=select.select(lis,[],[])
             for item in read:
                 try:
-                    s=item.recv(1024)
+                    s,addr=item.recvfrom(1024)
                     if s!='':
                         chunk=s                
-                        print str('')+':'+chunk
+                        print str('\n>>>')+':'+chunk
                 except:
                     traceback.print_exc(file=sys.stdout)
                     break
@@ -28,11 +28,11 @@ class Client(threading.Thread):
     def connect(self,host,port):
         self.sock.connect((host,port))
     def client(self,host,port,msg):               
-        sent=self.sock.send(msg)           
+        sent=self.sock.sendto(msg,(host,port))           
         #print "Sent\n"
     def run(self):
-        self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        #self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         try:
             host=raw_input("Enter the hostname\n>>")            
             port=int(raw_input("Enter the port\n>>"))
@@ -41,8 +41,8 @@ class Client(threading.Thread):
             return 1
         
         print "Connecting\n"
-        s=''
-        self.connect(host,port)
+        self.client(host,port,"!@#$!@#$%$#@!$#@!")
+        #self.connect(host,port)
         print "Connected\n"
         receive=self.sock
         time.sleep(1)
@@ -53,7 +53,7 @@ class Client(threading.Thread):
         srv.start()
         while 1:            
             #print "Waiting for message\n"
-            msg=raw_input('>>')
+            msg=raw_input()
             if msg=='exit':
                 break
             if msg=='':
@@ -62,6 +62,13 @@ class Client(threading.Thread):
             self.client(host,port,msg)
         return(1)
 if __name__=='__main__':
-    print "Starting client"
-    cli=Client()    
-    cli.start()
+    try:
+        print "Starting client"
+        cli=Client()
+        cli.setDaemon(True)    
+        print "CTRL+C to terminate"
+        cli.start()
+        signal.pause()
+    except(KeyboardInterrupt):
+        print "Exiting"
+        sys.exit()
--- Server.py
+++ Server.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 import socket
-import sys, traceback
+import sys, traceback, signal
 import threading
 import thread
 import select
@@ -10,62 +10,78 @@ TO_BE_SENT=[]
 SENT_BY={}
 class Server(threading.Thread):
 
+    
     def init(self):
-        self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.sock.bind(('',5535))
-        self.sock.listen(2)
-        SOCKET_LIST.append(self.sock)
+        #self.sock.listen(2)
+        #SOCKET_LIST.append(self.sock)
         print "Server started on port 5535"
 
+    def send_sock(self):
+        return self.sock
     def run(self):
         while 1:
-            read,write,err=select.select(SOCKET_LIST,[],[],0)     
-            for sock in read:
-                if sock==self.sock:                    
-                    sockfd,addr=self.sock.accept()
-                    print str(addr)
-                    SOCKET_LIST.append(sockfd)
-                    print SOCKET_LIST[len(SOCKET_LIST)-1]
+            #read,write,err=select.select(SOCKET_LIST,[],[],0)     
+#            for sock in read:
+#                if sock==self.sock:                    
+#                    sockfd,addr=self.sock.accept()
+#                    print str(addr)
+#                    SOCKET_LIST.append(sockfd)
+#                    print SOCKET_LIST[len(SOCKET_LIST)-1]
+#               else:
+            try:
+                s,addr=self.sock.recvfrom(1024)
+                if addr not in SOCKET_LIST:
+                    if s == "!@#$!@#$%$#@!$#@!":
+                        SOCKET_LIST.append(addr)
+                elif s=='':
+                    print str(addr)                           
+                    continue
                 else:
-                    try:
-                        s=sock.recv(1024)
-                        if s=='':
-                            print str(sock.getpeername())                            
-                            continue
-                        else:
-                            TO_BE_SENT.append(s)  
-                            SENT_BY[s]=(str(sock.getpeername()))
-                    except:
-                        print str(sock.getpeername())                    
+                    TO_BE_SENT.append(s)
+                    SENT_BY[s]=(str(addr))
+            except:
+                print str(addr)                    
                     
             
 class handle_connections(threading.Thread):
+    def get_sock(self,server):
+        self.sock=server.send_sock()
     def run(self):        
         while 1:
-            read,write,err=select.select([],SOCKET_LIST,[],0)
+            #read,write,err=select.select([],SOCKET_LIST,[],0)
             for items in TO_BE_SENT:
-                for s in write:
+                for s in SOCKET_LIST:
                     try:
-                        if(str(s.getpeername()) == SENT_BY[items]):
-                        	print("Ignoring %s"%(str(s.getpeername())))
+                        if(str(s) == SENT_BY[items]):
+                        	print("Ignoring %s"%(str(s)))
                         	continue
-                        print "Sending to %s"%(str(s.getpeername()))
-                        s.send(items)                                             
+                        print "Sending to %s"%(str(s))
+                        self.sock.sendto(str(s)+":"+items,s)                                             
                         
                     except:
                         traceback.print_exc(file=sys.stdout)
-                TO_BE_SENT.remove(items)   
-                del(SENT_BY[items])              
+                TO_BE_SENT.remove(items)
+                del(SENT_BY[items])         
                 
 
 
 if __name__=='__main__':
-    srv=Server()
-    srv.init()
-    srv.start()
-    print SOCKET_LIST
-    handle=handle_connections()    
-    handle.start()   
+    try:
+        srv=Server()
+        srv.setDaemon(True)
+        srv.init()
+        srv.start()
+        print SOCKET_LIST
+        handle=handle_connections()
+        handle.setDaemon(True) 
+        handle.get_sock(srv)   
+        handle.start()
+        print "CTRL+C to terminate\n"
+        signal.pause()
+    except(KeyboardInterrupt):
+        print "Exiting"
+        sys.exit()   
 
